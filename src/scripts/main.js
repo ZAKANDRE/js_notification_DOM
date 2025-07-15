6b5f679a359f02df6ecc57f99dbea1096aa5aa85
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-07-15 08:51:00 +0200

    commit after tests

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
-'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+
+  let blockType = document.createElement('section');
+  blockType.style.top = `${posTop}px`;
+  blockType.style.right = `${posRight}px`;
+  blockType.classList.add(`notification`);
+  blockType.classList.add(`${type}`);
+
+  document.querySelector('body').append(blockType);
+
+  let titleH2 = document.createElement("h2");
+  titleH2.classList.add("title");
+  titleH2.textContent = title;
+  blockType.append(titleH2);
+
+  let descriptionP = document.createElement("p");
+  descriptionP.textContent = description;
+  blockType.append(descriptionP);
+  setTimeout(() => {
+  blockType.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
